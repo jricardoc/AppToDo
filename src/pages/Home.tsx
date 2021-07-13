c408400767669a3839a9c7d1262c095ffc6879dc
Ricardo Carvalho <38188946+jricardoc@users.noreply.github.com>
2021-07-12 22:22:04 -0300

    Adicionando edição e alertas
    Adicionando edição e alertas

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,25 @@
 import React, { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Alert, StyleSheet, View } from 'react-native';
 
 import { Header } from '../components/Header';
 import { Task, TasksList } from '../components/TasksList';
 import { TodoInput } from '../components/TodoInput';
 
+export type EditTaskArgs = {
+  taskId: number;
+  taskNewTitle: string;
+}
+
 export function Home() {
   const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
+    const taskWithSameTitle = tasks.find(task => task.title === newTaskTitle)
+
+    if (taskWithSameTitle) {
+      return Alert.alert('Task já cadastrada', 'você não pode cadastrar task com o mesmo nome')
+    }
+
     const newTask = {
       id: new Date().getTime(),
       title: newTaskTitle,
@@ -27,30 +38,56 @@ export function Home() {
   function handleToggleTaskDone(id: number) {
     const updatedTasks = tasks.map(task => ({ ...task }));
 
-    const foundItem = updatedTasks.find(item => item.id === id);
+    const taskToBeMarkedAsDone = updatedTasks.find(task => task.id === id);
 
 
-    if (!foundItem)
+    if (!taskToBeMarkedAsDone)
       return;
 
-    foundItem.done = !foundItem.done;
+    taskToBeMarkedAsDone.done = !taskToBeMarkedAsDone.done;
     setTasks(updatedTasks);
-    
+
     // Dessa forma foi colocado um map para retornar um spred do objeto
     // Pesquisar Shallow Copy em JavaScript
     //TODO - toggle task done if exists
   }
 
   function handleRemoveTask(id: number) {
-    const updatedTasks = tasks.filter(task => task.id !== id);
+    Alert.alert('Remover item', 'Tem certeza que você deseja remover esse item?', [
+      {
+        style: 'cancel',
+        text: 'Não'
+      },
+      {
+        style: 'destructive',
+        text: 'Sim',
+        onPress: () => {
+          const updatedTasks = tasks.filter(task => task.id !== id);
 
-    setTasks(updatedTasks);
+          setTasks(updatedTasks);
+        }
+      }
+    ])
 
     // Se o id que está sendo recebido for o id que é diferente desta Task que está sendo passada agora,
     // ele irá retornar para o updatedTasks, se for igual, ele vai atender o filtro e não vai retornar. Ou seja
     // será apagado.
 
     //TODO - remove task from state
+  }
+
+  function handleEditTask({ taskId, taskNewTitle }: EditTaskArgs) {
+    const updatedTasks = tasks.map(task => ({ ...task }));
+
+    const taskToBeUpdated = updatedTasks.find(task => task.id === taskId);
+
+
+    if (!taskToBeUpdated)
+      return;
+
+    taskToBeUpdated.title = taskNewTitle;
+    
+    setTasks(updatedTasks);
   }
 
   return (
@@ -63,6 +100,7 @@ export function Home() {
         tasks={tasks}
         toggleTaskDone={handleToggleTaskDone}
         removeTask={handleRemoveTask}
+        editTask={handleEditTask}
       />
     </View>
   )
